97568ef5f8621cfee2d6f57a83db69a1ada5e60d
Ophir Back <ophir.back@broadcom.com>
2024-01-27 16:53:18 +0200

    setup improvement

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="google_code_discovery",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(
+        where="gcd"
+    ),
     install_requires=[
         'requests',
-        'html5lib'
+        'lxml',
+        'urllib3',
     ],
     license="MIT",
     entry_points = {
