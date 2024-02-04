0f11cebc0145fd7714bfd16c22841e9389bf79c1
Ophir Back <45718146+Kle0s@users.noreply.github.com>
2024-02-04 11:32:13 +0200

    Installation setup (#3)
    Co-authored-by: Ophir Back <ophir.back@broadcom.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="google_code_discovery",
+    name="google-code-discovery",
     version="0.0.1",
     packages=find_packages(
         where="gcd"
     ),
     install_requires=[
-        'requests',
-        'lxml',
-        'urllib3',
+        "requests",
+        "lxml",
+        "urllib3",
+        "enum34;python_version<'3.4'"
     ],
     license="MIT",
     entry_points = {
